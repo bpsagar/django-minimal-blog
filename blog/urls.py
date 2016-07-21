4b9710e9867c8e3b6180886f85951461cb78a221
Sagar Chakravarthy <bp.sagar@gmail.com>
2016-07-21 19:31:07 +0530

    Creating a foundation for writing flexible URLs.

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,18 @@
+from django.conf import settings
 from django.conf.urls import url
 
 from . import views
 
+# Making blog URLs flexible by updating the default URLs with custom URLS
+# Using this updated dictionary for generating URLs
+BLOG_URLS = {
+    'post_list': r'^posts/$',
+}
+BLOG_URLS.update(getattr(settings, 'BLOG_URLS', {}))
+
+
 urlpatterns = [
-    url(r'^blog/posts/$', views.PostListView.as_view(), name='post_list'),
+    # URL to display the list of paginated blog posts
+    url(BLOG_URLS['post_list'], views.PostListView.as_view(),
+        name='post_list'),
 ]
